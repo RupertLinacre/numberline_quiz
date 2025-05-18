e980a340b86fda9a1f41959f488a576f4a44ad8e
Robin Linacre <robinlinacre@hotmail.com>
2025-05-18 09:35:00 +0100

    deploy

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: '/numberline_quiz/', // Important: leading and trailing slash!
+    // If you have other build options, you can add them here
+    // build: {
+    //   outDir: 'dist', // This is the default, so not strictly necessary
+    // }
+});
